02b77e2f04ebd9116004508f1b5bdd75f3d96256
maleen <maleen.nj@gmail.com>
2021-09-16 12:33:50 +0530

    encoder new publish to ros using python

--- Motor/agv_odom/setup.py
+++ Motor/agv_odom/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-     packages=['ams_ekf'],
+     packages=['agv_odom'],
      package_dir={'': 'include'}
 )
 
--- Motor/agv_odom/src/agv_odom.py
+++ Motor/agv_odom/src/agv_odom.py
@@ -19,44 +19,102 @@ from scipy.interpolate import interp1d
 from scipy.interpolate import InterpolatedUnivariateSpline
 
 from std_msgs.msg import String
-from sensor_msgs.msg import NavSatFix
-from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
-
-from geometry_msgs.msg import PoseWithCovarianceStamped 
+from geometry_msgs.msg import Vector3Stamped
 import tf as tf
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray 
 
-from agv_odom import henc
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-   while not rospy.is_shutdown():
-       hello_str = "hello world %s" % rospy.get_time()
-       rospy.loginfo(hello_str)
-       pub.publish(hello_str)
-       rate.sleep()
+#from agv_odom import henc
+
+rospy.init_node('agv_odom', anonymous=True)
+current_encoder_left=0
+current_encoder_right=0
+previous_encoder_left=0
+previous_encoder_right=0
+
+current_time=0
+previous_time=0
+
+positionx=0
+positiony=0
+orientz=0
+
+odom_message=Odometry()
+odom_cnt=0
+
+wheel_radius = 0.048; #in m
+wheel_gap=0.37;
+ticks_per_rev = 2100;
+
+
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    global current_encoder_left, current_encoder_right, previous_encoder_left, previous_encoder_right, odom_cnt, odom_message, current_time, previous_time
+    global wheel_radius, wheel_gap,ticks_per_rev, positionx, positiony, orientz
+    #rospy.loginfo("Recieved %f", data.vector.x)
+    odom_broadcaster = tf.TransformBroadcaster()
+
+    previous_time=current_time
+    current_time=data.header.stamp
+
+    previous_encoder_left=current_encoder_left
+    previous_encoder_right=current_encoder_right
+
+
+    current_encoder_left=data.vector.x
+    current_encoder_right=data.vector.y
+
+    odom_pub = rospy.Publisher('agv_odometry', Odometry, queue_size=10)
+
+    left_dis_gap = np.float64((current_encoder_left-previous_encoder_left)/ticks_per_rev)*2*np.pi*wheel_radius;
+    right_dis_gap = np.float64((current_encoder_right-previous_encoder_right)/ticks_per_rev)*2*np.pi*wheel_radius;
+
+    deltarotz=(left_dis_gap-right_dis_gap)/wheel_gap
+    trans=(left_dis_gap+right_dis_gap)/2
+
+    deltax=trans*np.cos(deltarotz)
+    deltay=trans*np.sin(deltarotz)
+
+    positionx=positionx+deltax
+    positiony=positiony+deltay
+    orientz=orientz+deltarotz
+
+    odom_cnt = odom_cnt + 1
+
+    odom_quat = tf.transformations.quaternion_from_euler(0, 0, orientz)
+
+    odom_broadcaster.sendTransform(
+        (positionx, positiony, 0.),
+        odom_quat,
+        current_time,
+        "base_link",
+        "odom"
+    )
+
+
+    odom_message.header.stamp = current_time
+    odom_message.header.seq = odom_cnt
+
+    odom_message.header.frame_id = 'odom'
+    odom_message.pose.pose.position.x=positionx
+    odom_message.pose.pose.position.y=positiony
+
+    odom_pub.publish(odom_message)
+
+
     
-def listener():
+def encoder_listen():
 
-   # In ROS, nodes are uniquely named. If two nodes with the same
-   # name are launched, the previous one is kicked off. The
-   # anonymous=True flag means that rospy will choose a unique
-   # name for our 'listener' node so that multiple listeners can
-   # run simultaneously.
-   rospy.init_node('listener', anonymous=True)
 
-   rospy.Subscriber("chatter", String, callback)
+   rospy.Subscriber("encoder_ticks", Vector3Stamped, callback)
+   #encoder_pub()
 
    # spin() simply keeps python from exiting until this node is stopped
    rospy.spin()
 
 if __name__ == '__main__':
-  listener()
+
+    encoder_listen()
+    
+    
