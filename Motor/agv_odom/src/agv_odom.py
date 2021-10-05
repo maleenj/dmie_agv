ec0ed7c0c72eda23deaa9191ba33d97be506398f
maleen <maleen.nj@gmail.com>
2021-10-05 12:50:43 +0530

    Fixed odometry

--- Motor/agv_odom/src/agv_odom.py
+++ Motor/agv_odom/src/agv_odom.py
@@ -31,6 +31,10 @@ current_encoder_left=0
 current_encoder_right=0
 previous_encoder_left=0
 previous_encoder_right=0
+encoder_wraps_x=0
+encoder_wraps_y=0
+previous_tmpx=0
+previous_tmpy=0
 
 current_time=0
 previous_time=0
@@ -38,6 +42,8 @@ previous_time=0
 positionx=0
 positiony=0
 orientz=0
+increasex=0
+increasey=0
 
 odom_message=Odometry()
 odom_cnt=0
@@ -47,12 +53,95 @@ wheel_gap=0.37;
 ticks_per_rev = 2100;
 
 
+def wrapTo2Pi(angles):
+  
+  wrapped=(2*np.pi + angles) * (angles < 0) + angles*(angles >= 0)
+  
+  return wrapped
+
+def handle_dat_encoder(encoder_x,encoder_x_prev, encoder_y, encoder_y_prev):
+    global encoder_wraps_x, encoder_wraps_y, increasex, increasey
+
+    tmpx=encoder_x
+    tmpy=encoder_y
+
+    # process left
+    
+    # calculate diff
+    diff = encoder_x - encoder_x_prev
+
+    print("current x : ", encoder_x, "previous x :", encoder_x_prev, "diff : ", diff)
+
+
+    # check wrap around
+    if abs(diff) > (65000):
+        # if positive wrap around, increase
+        if encoder_x < encoder_x_prev:
+            print("Positive LEFT")
+            encoder_wraps_x = encoder_wraps_x + 1
+            increasex=1
+        # else decrease
+        else:
+            print("Negative LEFT")
+            encoder_wraps_x = encoder_wraps_x - 1
+            increasex=0
+                 
+
+    if abs(encoder_wraps_x) > 0:
+
+        if increasex==1:
+            tmpx = 32767*(2*encoder_wraps_x-1) + abs(-32768-encoder_x)
+        else:
+            tmpx = -32768*(2*encoder_wraps_x-1) - (32767-encoder_x) 
+
+    else:  
+        tmpx=encoder_x
+
+
+    print("tmpX : ", tmpx)
+
+    # process y
+
+    #calculate diff
+    diff = encoder_y - encoder_y_prev
+    #print("current y : ", encoder_y, "previous y :", encoder_y_prev, "diff : ", diff)
+
+    # check wrap around
+    if abs(diff) > (65000):
+        #if positive wrap around, increase
+        if encoder_y < encoder_y_prev:
+            print("Positive RIGHT")
+            encoder_wraps_y = encoder_wraps_y + 1
+            increasey=1
+
+        # else decrease
+        else:
+            print("Negative RIGHT")
+            encoder_wraps_y = encoder_wraps_y - 1
+            increasey=0
+            
+    if abs(encoder_wraps_y) > 0:
+
+        if increasey==1:
+            tmpy = 32767*(2*encoder_wraps_y-1) + abs(-32768-encoder_y)
+        else:
+            tmpy = -32768*(2*encoder_wraps_y-1) - (32767-encoder_y) 
+
+    else:  
+        tmpy=encoder_y
+    #print("tmpY : ", tmpy)
+
+    #print("encoder mean : ", encoder_mean)
+
+    return (tmpx, tmpy)
+
+
 
 
 def callback(data):
     global current_encoder_left, current_encoder_right, previous_encoder_left, previous_encoder_right, odom_cnt, odom_message, current_time, previous_time
-    global wheel_radius, wheel_gap,ticks_per_rev, positionx, positiony, orientz
-    rospy.loginfo("Recieved %f", data.vector.x)
+    global wheel_radius, wheel_gap,ticks_per_rev, positionx, positiony, orientz,previous_tmpx,previous_tmpy
+    #rospy.loginfo("Recieved %f, %f ", data.vector.x,data.vector.y)
     odom_broadcaster = tf.TransformBroadcaster()
 
     previous_time=current_time
@@ -65,20 +154,30 @@ def callback(data):
     current_encoder_left=data.vector.x
     current_encoder_right=data.vector.y
 
+    (tmpx,tmpy)=handle_dat_encoder(current_encoder_left,previous_encoder_left,current_encoder_right,previous_encoder_right)
+
     odom_pub = rospy.Publisher('agv_odometry', Odometry, queue_size=10)
 
-    left_dis_gap = np.float64((current_encoder_left-previous_encoder_left)/ticks_per_rev)*2*np.pi*wheel_radius;
-    right_dis_gap = np.float64((current_encoder_right-previous_encoder_right)/ticks_per_rev)*2*np.pi*wheel_radius;
+    left_dis_gap = np.float64((tmpx-previous_tmpx)/ticks_per_rev)*2*np.pi*wheel_radius;
+    right_dis_gap = np.float64((tmpy-previous_tmpy)/ticks_per_rev)*2*np.pi*wheel_radius;
 
-    deltarotz=(left_dis_gap-right_dis_gap)/wheel_gap
+    previous_tmpx=tmpx
+    previous_tmpy=tmpy
+
+
+
+    deltarotz=wrapTo2Pi((right_dis_gap-left_dis_gap)/wheel_gap)
     trans=(left_dis_gap+right_dis_gap)/2
 
-    deltax=trans*np.cos(deltarotz)
-    deltay=trans*np.sin(deltarotz)
+
+    
+    orientz=wrapTo2Pi(orientz+deltarotz)
+
+    deltax=trans*np.cos(orientz)
+    deltay=trans*np.sin(orientz)
 
     positionx=positionx+deltax
     positiony=positiony+deltay
-    orientz=orientz+deltarotz
 
     odom_cnt = odom_cnt + 1
 
@@ -99,6 +198,11 @@ def callback(data):
     odom_message.header.frame_id = 'odom'
     odom_message.pose.pose.position.x=positionx
     odom_message.pose.pose.position.y=positiony
+
+    odom_message.pose.pose.orientation.x=odom_quat[0]
+    odom_message.pose.pose.orientation.y=odom_quat[1]
+    odom_message.pose.pose.orientation.z=odom_quat[2]
+    odom_message.pose.pose.orientation.w=odom_quat[3]
 
     odom_pub.publish(odom_message)
 
